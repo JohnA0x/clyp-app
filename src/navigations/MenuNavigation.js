f7fc6a77534cc24fff08918b3f1656a7418ba111
gabrielawosusi <gabriel@clyppay.io>
2022-07-06 19:59:27 +0100

    Dev Commit

---
+++ src/navigations/MenuNavigation.js
@@ -0,0 +1,3 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
