964e2b84b91fee18b3717fd524b60cead4af32c5
gabrielawosusi <gabriel@clyppay.io>
2022-07-11 16:11:26 +0100

    Dev Commit

---
+++ src/styles/profile.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import { useFonts, Poppins_900Black, Poppins_700Bold, Poppins_600SemiBold, Poppins_400Regular } from "@expo-google-fonts/poppins";
+import * as Colors from '../constants/colors'
+
+
+export const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+      padding: 2,
+      backgroundColor: Colors.backgroundColor,
+    },
+
+    profileImage:{
+
+    },
+
+    profileName:{
+
+    },
+
+    editProfileButton:{
+
+    },
+})
