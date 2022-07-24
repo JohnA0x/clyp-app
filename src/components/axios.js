6cc54aadfd1e802524c602ea5e8b125d695d0933
John Alalade <john@clyppay.io>
2022-07-24 15:10:38 +0100

    Axios

---
+++ src/components/axios.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+axios.defaults.baseURL = 'http://localhost:5000/'
+// axios.defaults.baseURL = 'http://127.0.1.1/5000'
+axios.defaults.headers.common = {'Authorization': `Bearer ${AsyncStorage.getItem('token')}`}
+export default axios;
