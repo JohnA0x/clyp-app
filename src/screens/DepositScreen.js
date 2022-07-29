b61e69464041ee86000508d32cdaffdd21f136c9
gabrielawosusi <gabriel@clyppay.io>
2022-07-29 09:04:23 +0100

    Dev Commit

--- src/screens/DepositScreen.js
+++ src/screens/DepositScreen.js
@@ -1,4 +1,11 @@
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  TextInput,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styles } from "../styles/deposit";
@@ -17,44 +24,139 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { useState } from "react";
 import depositListArray from "../strings/depositlist";
+import cryptoListArray from "../strings/cryptolist";
 
-export default function DepositScreen({ navigation }) {
-  const preferencesList = ({ item }) => (
-    <View style={styles.rowContainer}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.push(item.name)}
-      >
-        <VectorButton
-          name={item.icon}
-          size={24}
-          color={Colors.primary}
-          style={styles.headerImage}
-        />
-        <Text style={styles.title}>{item.title}</Text>
-        <Text style={styles.subtitle}>{item.subtitle}</Text>
-      </TouchableOpacity>
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+export default function DepositScreen({ navigation, route }) {
+  const [cryptoName, setCryptoName] = useState("");
+  const [cryptoIcon, setCryptoIcon] = useState("");
+  const [walletOptions, setWalletOptions] = useState([
+    { address: "hh", abb: "hh" },
+  ]);
 
   return (
-    <SafeAreaView>
-      <View style={styles.header}>
-        <VectorButton
-          name="chevron-back"
-          size={24}
-          color={Colors.textColor}
-          style={styles.backButton}
-          handlePress={() => navigation.navigate(Strings.home)}
-        />
-        <Text style={styles.headerText}>{Strings.fund}</Text>
-      </View>
-      <FlatList
-        contentContainerStyle={styles.flatlist}
-        //ListEmptyComponent = { <Text>This List is a very Flat list</Text> }
-        data={depositListArray}
-        renderItem={preferencesList}
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen
+        name={Strings.deposit}
+        initialParams={route}
+        component={DepositOptions}
       />
-    </SafeAreaView>
+
+      <Stack.Screen
+        name={Strings.depositviaCrypto}
+        component={CryptoOptions}
+        initialParams={route}
+      />
+
+      <Stack.Screen
+        name="TransactionsOptions"
+        component={TransactionsOptions}
+        initialParams={route}
+      />
+    </Stack.Navigator>
   );
+
+  function DepositOptions({ navigation }) {
+    const depositOptions = ({ item }) => (
+      <View style={styles.rowContainer}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate(item.title)}
+        >
+          <VectorButton
+            name={item.icon}
+            size={24}
+            color={Colors.primary}
+            style={styles.headerImage}
+          />
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.subtitle}>{item.subtitle}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+
+    return (
+      <SafeAreaView>
+        <View style={styles.header}>
+          <VectorButton
+            name="chevron-back"
+            size={24}
+            color={Colors.textColor}
+            style={styles.backButton}
+            handlePress={() => navigation.navigate(Strings.home)}
+          />
+          <Text style={styles.headerText}>{Strings.fund}</Text>
+        </View>
+        <FlatList
+          //contentContainerStyle={styles.flatlist}
+          //ListEmptyComponent = { <Text>This List is a very Flat list</Text> }
+          data={depositListArray}
+          renderItem={depositOptions}
+        />
+      </SafeAreaView>
+    );
+  }
+
+  function CryptoOptions() {
+    const sendCryptoList = ({ item }) => (
+      <View style={styles.rowContainer}>
+        <TouchableOpacity
+          style={styles.list}
+          onPress={() => {
+            navigation.push("TransactionsOptions");
+            setCryptoName(item.name);
+            setCryptoIcon(item.icon);
+            setWalletOptions({ abb: item.abb });
+          }}
+        >
+          <Image source={{ uri: item.icon }} style={styles.image} />
+          <Text style={styles.valueText}>{item.value}</Text>
+          <Text style={styles.text}>{item.name}</Text>
+        </TouchableOpacity>
+      </View>
+    );
+    return (
+      <SafeAreaView>
+        <View style={styles.header}>
+          <VectorButton
+            name="chevron-back"
+            size={24}
+            color={Colors.textColor}
+            style={styles.backButton}
+            handlePress={() => navigation.navigate(Strings.home)}
+          />
+          <Text style={styles.headerText}>{Strings.fund}</Text>
+        </View>
+
+        <View style={styles.flatlist}>
+          <FlatList
+            data={cryptoListArray}
+            //ListHeaderComponent={renderHeader}
+            renderItem={sendCryptoList}
+          />
+        </View>
+      </SafeAreaView>
+    );
+  }
+
+  function TransactionsOptions() {
+    return (
+      <SafeAreaView>
+        <View style={styles.transactionAmountContainer}>
+          <Text style={styles.amountText}>Amount:</Text>
+          <TextInput style={styles.amountValueText}
+          keyboardType='numeric'
+          maxLength={12}
+          selectionColor={Colors.primary}
+          >0</TextInput>
+          <Text style={styles.amountMaxValue}>Max</Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
