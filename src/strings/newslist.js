8e3e55f3a5fd406f271de0c19ecb8706141eed01
gabrielawosusi <gabriel@clyppay.io>
2022-08-09 08:57:54 +0100

    Dev Commit

---
+++ src/strings/newslist.js
@@ -0,0 +1,30 @@
+import * as Strings from "../strings/strings";
+
+export const newsArrayList = [
+  {
+    id: 1,
+    title: 'Crypto went boom in 2022',
+    image: 'https://cdn-icons-png.flaticon.com/512/2964/2964063.png',
+    content: '',
+  },
+  {
+    id: 2,
+    title: 'Binance cut ties with China',
+    image: 'https://cdn-icons-png.flaticon.com/512/2964/2964063.png',
+    content: '',
+  },
+  {
+    id: 3,
+    title: 'Is RADAR the coin of the future?',
+    image: 'https://cdn-icons-png.flaticon.com/512/2964/2964063.png',
+    content: '',
+  },
+  {
+    id: 4,
+    title: 'US Inflation rises by 20%, affects crypto and stocks',
+    image: 'https://cdn-icons-png.flaticon.com/512/2964/2964063.png',
+    content: '',
+  },
+];
+
+export default newsArrayList;
