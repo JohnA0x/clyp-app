6cab60b660bdfd0b9849a4ed4c9fc3609f8a8a85
gabrielawosusi <gabriel@clyppay.io>
2022-08-21 19:20:09 +0100

    Theme Commit

---
+++ src/constants/theme.js
@@ -0,0 +1,18 @@
+import * as Colors from './colors';
+
+export const darkTheme = {
+    mode: 'dark',
+    primary: Colors.primaryDark,
+    secondary: Colors.secondaryDark,
+    tertiary: Colors.tertiaryDark,
+    text: Colors.textColorDark,
+
+}
+
+export const lightTheme = {
+    mode: 'light',
+    primary: Colors.primary,
+    secondary: Colors.secondary,
+    tertiary: Colors.tertiary,
+    text: Colors.textColor,
+}
